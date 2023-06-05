68aac4c90a1f50cecbb73a99382bda00df1dd76f
Mateja Cosovic <mateja@192.168.1.8>
2023-06-05 21:31:50 +0200

    feat: initial setup

---
+++ booking-accomodation/src/main/java/com/bookingaccomodation/BookingAccomodationApplication.java
@@ -0,0 +1,13 @@
+package com.bookingaccomodation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAccomodationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAccomodationApplication.class, args);
+	}
+
+}
