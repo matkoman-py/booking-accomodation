24c75e71059aaee853c7da594bee6417d887315f
djordjee55 <djordjenjegic1999@gmail.com>
2023-09-09 20:30:41 +0200

    fix: request paths

--- src/main/java/com/bookingaccomodation/controller/BookingController.java
+++ src/main/java/com/bookingaccomodation/controller/BookingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/booking")
+@RequestMapping("api/accommodation/booking")
 @RequiredArgsConstructor
 public class BookingController {
 
